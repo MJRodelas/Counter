eb5f85951d0b1869b0e370215eb986aab7859fbc
MJRodelas <34410253+MJRodelas@users.noreply.github.com>
2022-01-03 21:10:22 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+<script>
+    let countEl = document.getElementById("count-el")
+    console.log(countEl)
+    let count=0
+
+    function increment() {
+        count=count+1
+        countEl.innerText=count  
+    }
+</script>
