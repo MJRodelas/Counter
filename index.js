869c282bf430b47624e332fcd93111f5e6c22f2a
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-20 14:11:03 +0800

    Save count to previous entries
    // 1. Grab the save-el paragrah and store it in a variable called saveEl
    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
    // 3. Render the variable in the saveEl using innerText
    // NB: Make sure to not delete the existing content of the paragraph

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 //Increment the count variable when the button is clicked (log it out)
 // Change the count-el in the HTML to reflect the new count
 
+
 let countEl = document.getElementById("count-el")
+
+
+let saveEl = document.getElementById("save-el")
 
 let count = 0
 
@@ -12,10 +16,13 @@ function increment(){
     countEl.innerText = count
 }
 
-increment()
-
 //Create a function save(), which logs the count when it's called
 function save(){
-    console.log(count)
-
+    let saveCount = count + " - "
+    saveEl.innerText += saveCount
 }
+
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+// 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+// 3. Render the variable in the saveEl using innerText
+// NB: Make sure to not delete the existing content of the paragraph
