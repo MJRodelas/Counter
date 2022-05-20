c82d022545209328f47126d5ec6061ff2399487c
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-20 13:43:01 +0800

    Plus Equal technique in JS
    //Change this to use the plus equal technique you've learned

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment(){
-    count = count + 1
+    count += 1
     countEl.innerText = count
 }
 
---
+++ stringNotif.js
@@ -0,0 +1,12 @@
+//create a variable message that storee the string "You have a message"
+
+let username = "MJ"
+let message = "You have a message"
+
+console.log(message + ", " + username + "!")
+
+//Create a variable messageToUser, that contains the message we have logged
+
+let messageToUser = message + ", " + username + "!"
+
+console.log(messageToUser)
