b4b4ed93c66db1d29eb207a85d0e4e516e1a382f
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-20 11:45:19 +0800

    Counter with Increment Function
    Interactive Counter

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 
-document.getElementById("count-el").innerText=5
+// document.getElementById("count-el").innerText=5
+//  let firstBatch = 5
+// let secondBath = 7
+// let count = firstBatch + secondBath
+// console.log(count)
+
+
+
+// function increment(){
+//     console.log("The button was click")
+// }
+
+
+// function myLogger(){
+//     console.log(42)
+  
+// }
+
+// myLogger()
+
+//Initialize the count as 0
+//Listen for clicks on the increment button
+//Increment the count variable when the button is clicked (log it out)
+// Change the count-el in the HTML to reflect the new count
+
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+
+let count = 0
+
+function increment(){
+    count = count + 1
+    countEl.innerText = count
+}
+
+increment()
