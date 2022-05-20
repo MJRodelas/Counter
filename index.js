cfd0226e3d7e2bd59e47e63c51edab1305ed3dc2
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-19 17:50:00 -0700

    Made Css, JS
    Introduction to Javascript

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-<script>
-    let countEl = document.getElementById("count-el")
-    console.log(countEl)
-    let count=0
 
-    function increment() {
-        count=count+1
-        countEl.innerText=count  
-    }
-</script>
+document.getElementById("count-el").innerText=5
