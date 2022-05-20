dd205786cfd237786689d9209a287665031bfcc2
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-20 12:20:11 +0800

    Adding save button
    Save button was added using function

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-
-// document.getElementById("count-el").innerText=5
-//  let firstBatch = 5
-// let secondBath = 7
-// let count = firstBatch + secondBath
-// console.log(count)
-
-
-
-// function increment(){
-//     console.log("The button was click")
-// }
-
-
-// function myLogger(){
-//     console.log(42)
-  
-// }
-
-// myLogger()
-
 //Initialize the count as 0
 //Listen for clicks on the increment button
 //Increment the count variable when the button is clicked (log it out)
 // Change the count-el in the HTML to reflect the new count
 
 let countEl = document.getElementById("count-el")
-console.log(countEl)
 
 let count = 0
 
@@ -35,3 +13,9 @@ function increment(){
 }
 
 increment()
+
+//Create a function save(), which logs the count when it's called
+function save(){
+    console.log(count)
+
+}
